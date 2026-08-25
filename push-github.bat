f1d716f660a17517f933bb119b150fe594c5ab5b
LucasTI <TI@LUCAS-TI.santacasadepiracicaba.com.br>
2026-08-25 16:59:17 -0300

    feat: add push-github.bat helper script

---
+++ push-github.bat
@@ -0,0 +1,12 @@
+@echo off
+title Enviar JTech Fit para o GitHub
+echo ========================================================
+echo Enviando projeto JTech Fit para https://github.com/lucasjustinosousa/JTech-Fit.git
+echo ========================================================
+echo.
+git push -u origin main
+echo.
+echo ========================================================
+echo Concluido! Pressione qualquer tecla para fechar.
+echo ========================================================
+pause
