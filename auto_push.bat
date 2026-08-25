a7e9bf76dc9c3f892b622bbbecf76b7e1386d21c
LucasTI <TI@LUCAS-TI.santacasadepiracicaba.com.br>
2026-08-25 17:29:02 -0300

    "

--- auto_push.bat
+++ auto_push.bat
@@ -5,23 +5,21 @@ echo   JTech Fit - Sincronizacao automatica Git & Vercel
 echo ========================================================
 echo.
 
-set /p MSG="Digite a mensagem do commit (ou pressione ENTER para mensagem padrao): "
-if "%MSG%"=="" set MSG="update: alteracoes do aplicativo JTech Fit"
+set MSG=%~1
+if "%MSG%"=="" (
+    set /p MSG="Digite a mensagem do commit (ou pressione ENTER para mensagem padrao): "
+)
+if "%MSG%"=="" set MSG="update: alteracoes no aplicativo JTech Fit"
 
-echo.
 echo 1. Adicionando arquivos modificados...
 git add .
 
-echo.
-echo 2. Criando commit com a mensagem: %MSG%
-git commit -m %MSG%
+echo 2. Criando commit...
+git commit -m "%MSG%"
 
-echo.
 echo 3. Enviando para o GitHub e Vercel (Auto Deploy)...
 git push origin main
 
-echo.
 echo ========================================================
 echo   SUCESSO! Alteracoes enviadas para o GitHub e Vercel.
 echo ========================================================
-pause
